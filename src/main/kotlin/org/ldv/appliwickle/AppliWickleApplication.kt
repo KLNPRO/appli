017afadf3248050f336edf0f2d53bb5069886e4f
fred5551 <fredericbitsindou246@gmail.com>
2025-11-10 09:21:30 +0100

    firstcommit

---
+++ src/main/kotlin/org/ldv/appliwickle/AppliWickleApplication.kt
@@ -0,0 +1,11 @@
+package org.ldv.appliwickle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppliWickleApplication
+
+fun main(args: Array<String>) {
+    runApplication<AppliWickleApplication>(*args)
+}
