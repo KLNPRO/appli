605bcbd3c0f23b1a3677f4c07475c5a052d9ad39
killian <killianemmanuel68@gmail.com>
2025-11-21 15:22:47 +0100

    17 commit

--- src/main/kotlin/org/ldv/appliwickle/controllers/MainController.kt
+++ src/main/kotlin/org/ldv/appliwickle/controllers/MainController.kt
@@ -29,7 +29,6 @@ class MainController {
     }
     @GetMapping("/wickle/connexion")
     fun connexion(@RequestParam error: Boolean?, model: Model): String {
-        // Ajoute un attribut "error" au modèle si la requête contient une erreur
         model.addAttribute("error", error == true)
         return "pageVisiteur/connexion"
     }
